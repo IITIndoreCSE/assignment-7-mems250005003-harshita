afbd0ef312b2aa1d79beade35463623279dd22ee
mems250005003-harshita <mems250005003@iiti.ac.in>
2025-10-17 16:05:51 +0530

    Update Q4.cpp

--- Q4.cpp
+++ Q4.cpp
@@ -1,1 +1,34 @@
 /* Write a function that finds that first element that repeats itself in an array. */
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter " << n << " elements:" << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    unordered_set<int> seen;
+    int firstRepeat = -1;
+
+    for (int i = 0; i < n; i++) {
+        if (seen.find(arr[i]) != seen.end()) {
+            firstRepeat = arr[i];
+            break;
+        }
+        seen.insert(arr[i]);
+    }
+
+    if (firstRepeat == -1)
+        cout << "No repeating elements found." << endl;
+    else
+        cout << "First repeating element is: " << firstRepeat << endl;
+
+    return 0;
+}
